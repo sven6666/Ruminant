056708a9596f526bd141137d080b7b4c17594bc4
Sven Rappo <sven.rappo@students.gymneufeld.ch>
2019-07-02 14:10:47 +0200

    Flask2

--- Ruminant/src/rumimant.py
+++ Ruminant/src/rumimant.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, url_for, redirect, session
-from flask.ext.mysqldb import MySQL
+from flask_mysqldb import MySQL
 app=Flask(__name__)
 
+
+mysql = MySQL(app)
 @app.route("/")
 def eShop():
     return redirect(url_for('home',))
@@ -17,8 +19,16 @@ def Waren():
 @app.route("/login",methods=['GET','POST'])
 def login():
     if request.method=='POST':
-        loggedIn=true;
-        return redirect(url_for('loggedIn',))
+        session['uname']=request.form['username']
+        session['pword']=request.form['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute(’’’SELECT username,password FROM Login’’’)
+        logindata=cursor.fetchall()
+        mysql.connection.commit()
+        cursor.close
+        if logindata(1)==session.get('uname') and logindata(2)==session.get('pword'):
+            loggedIn=true;
+            return redirect(url_for('Waren',))
     return render_template('Login.html',message=message)
 
 
